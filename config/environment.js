ba1cf79db6dff8779f386627c2ff01b603dbb523
vadimD <dashchenko.vad.92@gmail.com>
2020-02-27 12:20:41 +0200

    added new components and tests files

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmFkaW1kIiwiYSI6ImNrNzRqbnhnMTBsMXEza2w5NWdvZXFndjYifQ.Oit-7BkviR-cqDQ5zKZEAA';
+
   return ENV;
 };
