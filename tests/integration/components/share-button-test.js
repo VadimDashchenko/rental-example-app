d0c4185a3c79a248f74e25a9125b12dc5f6bf83c
vadimD <dashchenko.vad.92@gmail.com>
2020-02-28 15:11:25 +0200

    added some code for testing share-button

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -1,26 +1,35 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import {module, test} from 'qunit';
+import {setupRenderingTest} from 'ember-qunit';
+import Service from '@ember/service';
+import {render} from '@ember/test-helpers';
+import {hbs} from 'ember-cli-htmlbars';
 
-module('Integration | Component | share-button', function(hooks) {
+class MockRouterService extends Service {
+  get currentURL() {
+    return '/foo/bar?baz=true#some-section';
+  }
+}
+
+module('Integration | Component | share-button', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.owner.register('service:router', MockRouterService);
+  });
 
-    await render(hbs`<ShareButton />`);
+  test('basic usage', async function (assert) {
+    await render(hbs`<ShareButton>Tweet this!</ShareButton>`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <ShareButton>
-        template block text
-      </ShareButton>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('a').exists();
+    assert.dom('a').hasAttribute('target', '_blank');
+    assert.dom('a').hasAttribute('rel', 'external nofollow noopener noreferrer');
+    assert.dom('a').hasAttribute('href', `https://twitter.com/intent/tweet?url=${
+      encodeURIComponent(
+        new URL('/foo/bar?baz=true#some-section', window.location.origin)
+      )
+    }`);
+    assert.dom('a').hasClass('share');
+    assert.dom('a').hasClass('button');
+    assert.dom('a').containsText('Tweet this!');
   });
 });
