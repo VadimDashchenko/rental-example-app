0c7a5412403b4df06532efc6c6f2c5ce6d304ab5
vadimD <dashchenko.vad.92@gmail.com>
2020-03-02 11:11:03 +0200

    added adapters and serializers

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,27 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = [
-  'Condo',
-  'Townhouse',
-  'Apartment'
-];
 
 export default class RentalRoute extends Route {
-  async model(params) {
-    let response = await fetch(`api/rental/${params.rental_id}.json`);
-    let {data} = await response.json();
-
-    return data.map(model => {
-      let {attributes, id} = model;
-      let type;
-
-      if(COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone'
-      }
-
-      return {id, type, ...attributes}
-    });
-  }
+ @service store;
+ async model() {
+   return this.store.find('rental', params.rental_id);
+ }
 }
